3c51b384f7dc3215870f594852d48fa1a663c044
Sunil Yadav <118260976+sunilyadavitm@users.noreply.github.com>
2023-07-18 16:44:02 +0530

    Update and rename PPT_Assignment_5 to PPT_Assignment_5.java

---
+++ PPT_Assignment_5.java
@@ -0,0 +1,47 @@
+// **Q1.What is Exception in Java?**
+
+Ans.
+
+  
+//**Q2.What is Exception Handling?**
+
+Ans.
+
+
+//**Q3.What is the difference between Checked and Unchecked Exceptions and Error?**
+
+Ans.
+
+
+//**Q4.What are the difference between throw and throws in Java?**
+
+Ans.
+
+
+//**Q5.What is multithreading in Java? mention its advantages**
+
+Ans.
+
+
+//**Q6.Write a program to create and call a custom exception**
+
+Ans.
+
+
+//**Q7.How can you handle exceptions in Java?**
+
+Ans.
+
+//**Q8.What is Thread in Java?**
+
+Ans.
+
+
+//**Q9. What are the two ways of implementing thread in Java?**
+
+Ans.
+
+
+//**Q10.What do you mean by garbage collection?**
+
+Ans.
